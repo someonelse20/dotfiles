7408686697f859899499411bf41d397bbc67a2ad
Someonelse <kennethmcclung@gmail.com>
2026-09-03 13:19:40 -0400

    added desktop hypr conf

--- hypr/machines/desktop.lua
+++ hypr/machines/desktop.lua
@@ -1,6 +1,6 @@
 -- autostart
 hl.on("hyprland.start", function()
 	hl.exec_cmd("waybar")
-	hl.exec_cmd("[workspace 11] waterfox")
+	hl.exec_cmd("[workspace 11] librewolf")
 	hl.exec_cmd("[workspace 1] kitty")
 end)
